c7cbc421f5a75b73658ddf2d697b9bb2900e8fb4
renaaina <renaaina@renaaina>
2025-05-12 09:25:26 +0400

    ajout des modes de difficultes

--- plus_moins.c
+++ plus_moins.c
@@ -2,20 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
+int mode;
+int essais_max:
+int essais = 0;
+
+printf("choisissez un mode de difficulte ( 0 = facile, 1 = moyen,
+2 =difficile) : ");
+scanf("%d" , &mode);
+
+switch (mode) {
+    case 0: essais_max = -1;
+break;
+    case 1: essais_max = 25; break;
+    case 2; essais_max = 10; break;
+    default; essais_max = -1;
+}
 int main(){
      srand(time(NULL)); //initialise le generateur
      int valeur_a_trouver;
      int choix_joueur;
-     
-     printf("Valeur a deviner generee (pour debogage) : %d\n",
-valeur_a_trouver)";
 
-     printf("Votre choix : "):
-     scanf("%d" , &choix_joueur);
-
-     plus_moins(choix_joueur , valeur_a_trouver);
-
-     return 0;
+     do {
+          printf("Votre choix : "):
+          scanf("%d" , &choix_joueur);
+          plus_moins(choix_joueur , valeur_a_trouver);
+	  essais++;
+	  if (essais_max != -1 && essais >+ essais_max) {
+	     printf("Nombre d`essais depasse.Perdu !\n");
+	     break;
+	  }
+     } while (choix_joueur != valeur_a_trouver);
 }
 
 void plus_moins(int choix_joueur, int valeur_a_trouver) {
